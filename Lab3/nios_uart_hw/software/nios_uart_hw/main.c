ec04d48d320a939800b29bbd195eca741d3c7713
Luca Sasselli <4eversax@gmail.com>
2016-12-15 21:12:22 +0100

    Added MAX10 test

--- Lab3/nios_uart_hw/software/nios_uart_hw/main.c
+++ Lab3/nios_uart_hw/software/nios_uart_hw/main.c
@@ -3,41 +3,41 @@
 #include <altera_avalon_pio_regs.h>
 
 int read(int port){
-	return IORD_ALTERA_AVALON_PIO_DATA(port) & 0x01;
+    return IORD_ALTERA_AVALON_PIO_DATA(port) & 0x01;
 }
 
 void write(int port, int val){
-	IOWR_ALTERA_AVALON_PIO_DATA(port, val);
+    IOWR_ALTERA_AVALON_PIO_DATA(port, val);
 }
 
 int edge_read(int port){
-	return IORD_ALTERA_AVALON_PIO_EDGE_CAP(port) & 0x01;
+    return IORD_ALTERA_AVALON_PIO_EDGE_CAP(port) & 0x01;
 }
 
 void edge_write(int port, int val){
-	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(port, val);
+    IOWR_ALTERA_AVALON_PIO_EDGE_CAP(port, val);
 }
 
 int main()
 { 
-  write(PIO_1_BASE, 0x000A);
-  write(PIO_0_BASE, 0x0A2B);
-  edge_write(PIO_3_BASE, 1);
+    write(PIO_1_BASE, 0x000A);
+    write(PIO_0_BASE, 0x0A2B);
+    edge_write(PIO_3_BASE, 1);
 
-  alt_putstr("READY!\n");
+    alt_putstr("READY!\n");
 
-  while (1){
-	  if(edge_read(PIO_3_BASE)){
-		  alt_printf("RX: ");
-		  int payload = IORD_ALTERA_AVALON_PIO_DATA(PIO_2_BASE);
-		  int i;
-		  for(i=2; i< 11; i++){
-			alt_printf("%i",payload%2);
-			payload = payload >> 1;
-		  }
-		  alt_printf("\n");
-		  edge_write(PIO_3_BASE, 1);
-	  }
-  }
-  return 0;
+    while (1){
+        if(edge_read(PIO_3_BASE)){
+            alt_printf("RX: ");
+            int payload = IORD_ALTERA_AVALON_PIO_DATA(PIO_2_BASE);
+            int i;
+            for(i=2; i< 11; i++){
+                alt_printf("%i",payload%2);
+                payload = payload >> 1;
+            }
+            alt_printf("\n");
+            edge_write(PIO_3_BASE, 1);
+        }
+    }
+    return 0;
 }
